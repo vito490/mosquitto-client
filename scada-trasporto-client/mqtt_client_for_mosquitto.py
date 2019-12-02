75817f770608c7348700507edd757cb2d4961dd5
vito.sammarco1 <vito.sammarco1@ibm.com>
2019-12-02 11:03:37 +0100

    primo commit

---
+++ scada-trasporto-client/mqtt_client_for_mosquitto.py
@@ -0,0 +1,43 @@
+-----------------------------------------------------------------------------------------
+# mqtt_client v. 8/11/2019
+# usa Eclipse PAHO MQTT Lib
+#------------------------------------------------------------------------------------------
+import paho.mqtt.client as mqtt #import module
+import time
+import sys
+#----------------------------------------------------------------------------------------
+bridge_address="mosquitto.1544-d-iot-data-platform-1544.svc.cluster.local:1883"
+#-----------------------------------------------------------------------------------------
+
+# Callback function per il client BRIDGE
+#-----------------------------------------------------------------------------------------
+def on_connect_bridge(client, userdata, flags, rc):
+    if rc==0:
+        print("bridgeclient connected OK")
+    else:
+        print("bridgeclient Bad connection=",rc)
+#-----------------------------------------------------------------------------------------
+def on_message_bridge(client, userdata, message):
+    print("message received from BRIDGE, topic: "+message.topic+", payload: " +str(message.payload.decode("utf-8")))
+#-----------------------------------------------------------------------------------------
+def on_publish_bridge(client, userdata, message):
+    print("Publishing message to BRIDGE, topic: "+message.topic+", payload: " +str(message.payload.decode("utf-8")))
+#-----------------------------------------------------------------------------------------
+print("creating bridge client")
+bridgeclient = mqtt.Client("BRIDGECLIENT") #create new client instance
+bridgeclient.on_message=on_message_bridge #attach messaging callback
+bridgeclient.on_publish=on_publish_bridge #attach publishing callback
+bridgeclient.on_connect=on_connect_bridge #attach connecting callback
+print("connecting bridge client")
+bridgeclient.connect(bridge_address) #connect to bridge
+print("Subscribing bridgeclient to all topics")
+bridgeclient.subscribe("#")  #riceve tutti i messaggi del server BRIDGE
+bridgeclient.loop_start() #rimane in attesa sulla porta di connessionE
+i = 1
+while i < 30:
+	print(i)
+	i += 1
+	newTopic="devices/smartpipe/messages/events/dev="+i;
+	message = "new message #"+i
+	ret= bridgeclient.publish(newTopic,str(message.decode("utf-8")))
+	print("messaggio inviato con risposta"+ret)
---
+++ smart-pipe-client/mqtt_client.py
@@ -0,0 +1,53 @@
+-----------------------------------------------------------------------------------------
+# mqtt_client v. 8/11/2019
+# usa Eclipse PAHO MQTT Lib
+# Legge da SMARTPIPE MQTT server e scrive su BRIDGE MQTT server
+#------------------------------------------------------------------------------------------
+import paho.mqtt.client as mqtt #import module
+import time
+import sys
+#----------------------------------------------------------------------------------------
+bridge_address="XX.XXX.XX.XXX"
+pipe_address="YY.YYY.YY.YYY"
+#-----------------------------------------------------------------------------------------
+# Callback function per il client SMARTPIPE
+#
+def on_message_pipe(client, userdata, message):
+    print("message received from SMARTPIPE, topic: "+message.topic+", payload: " +str(message.payload.decode("utf-8")))
+    splitTopic = message.topic.split("/")
+    newTopic="devices/smartpipe/messages/events/dev="+splitTopic[1];
+    ret= bridgeclient.publish(newTopic,str(message.payload.decode("utf-8")))
+#-----------------------------------------------------------------------------------------
+# Callback function per il client BRIDGE
+#-----------------------------------------------------------------------------------------
+def on_connect_bridge(client, userdata, flags, rc):
+    if rc==0:
+        print("bridgeclient connected OK")
+    else:
+        print("bridgeclient Bad connection=",rc)
+#-----------------------------------------------------------------------------------------
+def on_message_bridge(client, userdata, message):
+    print("message received from BRIDGE, topic: "+message.topic+", payload: " +str(message.payload.decode("utf-8")))
+#-----------------------------------------------------------------------------------------
+def on_publish_bridge(client, userdata, message):
+    print("Publishing message to BRIDGE, topic: "+message.topic+", payload: " +str(message.payload.decode("utf-8")))
+#----------------------------------------------------------------------------------------
+print("creating bridge client")
+bridgeclient = mqtt.Client("BRIDGECLIENT") #create new client instance
+bridgeclient.on_message=on_message_bridge #attach messaging callback
+bridgeclient.on_publish=on_publish_bridge #attach publishing callback
+bridgeclient.on_connect=on_connect_bridge #attach connecting callback
+print("connecting bridge client")
+bridgeclient.connect(bridge_address) #connect to bridge
+print("Subscribing bridgeclient to all topics")
+bridgeclient.subscribe("#")  #riceve tutti i messaggi del server BRIDGE
+bridgeclient.loop_start() #rimane in attesa sulla porta di connessione
+#----------------------------------------------------------------------------------------
+print("creating smartpipe client")
+pipeclient = mqtt.Client("PIPECLIENT") #create new instance
+pipeclient.on_message=on_message_pipe #attach messaging callback for pipeclient
+print("connecting smartpipe client")
+pipeclient.connect(pipe_address) #connect to pipe broker
+print("Subscribing pipeclient to smartpipe/# topics")
+pipeclient.subscribe("smartpipe/#") #riceve dalla coda smartpipe solo i messaggi del tipo smartpipe/xxxx
+pipeclient.loop_start()
